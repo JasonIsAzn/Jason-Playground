421edf02d5639fd67b8b0aef924f5f306d8da494
JasonIsAzn <lejason123@gmail.com>
2023-05-26 16:33:22 -0500

    about expansion - dropdown menu and images

--- src/components/Landing/About.tsx
+++ src/components/Landing/About.tsx
@@ -1,11 +1,20 @@
 import { useState } from 'react'
+import { Fragment } from 'react'
+import { Menu, Transition } from '@headlessui/react'
+import { Ripple, initTE } from "tw-elements";
 
+initTE({ Ripple });
 
 const About = () => {
     const [isHovered, setIsHovered] = useState(true);
     const [open, setOpen] = useState(false);
 
     console.log(open)
+
+    function classNames(...classes: any) {
+        return classes.filter(Boolean).join(' ')
+    }
+
 
     return (
         <div id="section-about" className='p-2 h-full pb-24 bg-[#F5F5F5]'>
@@ -39,7 +48,116 @@ const About = () => {
                 {/* Expanded Section */}
                 {open && (
                     <div className='col-span-11 col-start-2'>
-                        <div className='col-start-2 flex justify-center items-center mt-48 md:text-4xl lg:text-4xl'></div>
+                        <div className='col-start-2 flex justify-center items-center mt-48 md:text-4xl'>
+                            <Menu as="div" className="relative inline-block text-left">
+                                <Menu.Button className="">
+                                    <button
+                                        className="flex items-center border-b border-black whitespace-nowrap bg-[#F5F5F5] px-6 pl-2 pb-2 pt-2.5 leading-normal text-neutral-800 shadow-[0_4px_9px_-4px_#fbfbfb] transition duration-150 ease-in-out hover:bg-neutral-100 hover:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.3),0_4px_18px_0_rgba(251,251,251,0.2)] focus:bg-neutral-100 focus:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.3),0_4px_18px_0_rgba(251,251,251,0.2)] focus:outline-none focus:ring-0 active:bg-neutral-200 active:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.3),0_4px_18px_0_rgba(251,251,251,0.2)] motion-reduce:transition-none"
+                                        type="button"
+                                        aria-expanded="false"
+                                        data-te-ripple-init>
+                                        <span className="mr-6 w-2">
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 20 20"
+                                                fill="currentColor"
+                                                className="h-5 w-5">
+                                                <path
+                                                    fill-rule="evenodd"
+                                                    d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                                                    clip-rule="evenodd" />
+                                            </svg>
+                                        </span>
+
+                                        Pick a topic
+                                    </button>
+                                </Menu.Button>
+
+                                <Transition
+                                    as={Fragment}
+                                    enter="transition ease-out duration-100"
+                                    enterFrom="transform opacity-0 scale-95"
+                                    enterTo="transform opacity-100 scale-100"
+                                    leave="transition ease-in duration-75"
+                                    leaveFrom="transform opacity-100 scale-100"
+                                    leaveTo="transform opacity-0 scale-95"
+                                >
+                                    <Menu.Items className="absolute right-0 z-10 mt-2 w-64 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                        <div className="py-1">
+
+                                            <Menu.Item>
+                                                {({ active }) => (
+                                                    <div
+                                                        className={classNames(
+                                                            active ? 'cursor-pointer bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                            'flex justify-center items-center text-3xl px-4 py-2'
+                                                        )}
+                                                        onClick={() => console.log('Anime')}
+                                                    >
+                                                        Anime
+                                                    </div>
+                                                )}
+                                            </Menu.Item>
+
+                                            <Menu.Item>
+                                                {({ active }) => (
+                                                    <div
+                                                        className={classNames(
+                                                            active ? 'cursor-pointer bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                            'flex justify-center items-center text-3xl px-4 py-2'
+                                                        )}
+                                                        onClick={() => console.log('Artist')}
+
+                                                    >
+                                                        Artist
+                                                    </div>
+                                                )}
+                                            </Menu.Item>
+
+                                            <Menu.Item>
+                                                {({ active }) => (
+                                                    <div
+                                                        className={classNames(
+                                                            active ? 'cursor-pointer bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                            'flex justify-center items-center text-3xl px-4 py-2'
+                                                        )}
+                                                        onClick={() => console.log('Gym')}
+
+                                                    >
+                                                        Gym
+                                                    </div>
+                                                )}
+                                            </Menu.Item>
+
+                                        </div>
+                                    </Menu.Items>
+                                </Transition>
+                            </Menu>
+                            <div> about my personal interest</div>
+                        </div>
+                        <div className='col-span-10 col-start-2 flex flex-row justify-center items-center mt-24 space-x-16 mx-[300px]'>
+
+                            <div className="flex flex-wrap justify-center">
+                                <div className="">
+                                    <img src="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/img/team-2-800x800.jpg" alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+                                </div>
+                            </div>
+
+                            <div className="flex flex-wrap justify-center">
+                                <div className="">
+                                    <img src="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/img/team-2-800x800.jpg" alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+                                </div>
+                            </div>
+
+
+                            <div className="flex flex-wrap justify-center">
+                                <div className="">
+                                    <img src="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/img/team-2-800x800.jpg" alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+                                </div>
+                            </div>
+
+
+                        </div>
                     </div>
                 )}
 
--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,8 +1,104 @@
+import { Fragment } from 'react'
+import { Menu, Transition } from '@headlessui/react'
 
-function Test() {
-    return (
-        <div></div>
-    )
+// Initialization for ES Users
+import {
+    Ripple,
+    initTE,
+} from "tw-elements";
+
+initTE({ Ripple });
+
+function classNames(...classes: any) {
+    return classes.filter(Boolean).join(' ')
 }
 
-export default Test;
+export default function Test() {
+    return (
+        <div className="">
+            <Menu as="div" className="relative inline-block text-left">
+                <Menu.Button className="">
+                    <button
+                        className="flex items-center whitespace-nowrap rounded bg-neutral-50 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-neutral-800 shadow-[0_4px_9px_-4px_#fbfbfb] transition duration-150 ease-in-out hover:bg-neutral-100 hover:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.3),0_4px_18px_0_rgba(251,251,251,0.2)] focus:bg-neutral-100 focus:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.3),0_4px_18px_0_rgba(251,251,251,0.2)] focus:outline-none focus:ring-0 active:bg-neutral-200 active:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.3),0_4px_18px_0_rgba(251,251,251,0.2)] motion-reduce:transition-none"
+                        type="button"
+                        aria-expanded="false"
+                        data-te-ripple-init>
+                        Light
+                        <span className="ml-2 w-2">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                                className="h-5 w-5">
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </span>
+                    </button>
+                </Menu.Button>
+
+                <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                >
+                    <Menu.Items className="absolute right-0 z-10 mt-2 w-24 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        <div className="py-1">
+
+                            <Menu.Item>
+                                {({ active }) => (
+                                    <div
+                                        className={classNames(
+                                            active ? 'cursor-pointer bg-gray-100 text-gray-900' : 'text-gray-700',
+                                            'block px-4 py-2 text-sm'
+                                        )}
+                                        onClick={() => console.log('Anime')}
+                                    >
+                                        Anime
+                                    </div>
+                                )}
+                            </Menu.Item>
+
+                            <Menu.Item>
+                                {({ active }) => (
+                                    <div
+                                        className={classNames(
+                                            active ? 'cursor-pointer bg-gray-100 text-gray-900' : 'text-gray-700',
+                                            'block px-4 py-2 text-sm'
+                                        )}
+                                        onClick={() => console.log('Artist')}
+
+                                    >
+                                        Artist
+                                    </div>
+                                )}
+                            </Menu.Item>
+
+                            <Menu.Item>
+                                {({ active }) => (
+                                    <div
+                                        className={classNames(
+                                            active ? 'cursor-pointer bg-gray-100 text-gray-900' : 'text-gray-700',
+                                            'block px-4 py-2 text-sm'
+                                        )}
+                                        onClick={() => console.log('Gym')}
+
+                                    >
+                                        Gym
+                                    </div>
+                                )}
+                            </Menu.Item>
+
+                        </div>
+                    </Menu.Items>
+                </Transition>
+            </Menu>
+        </div>
+    )
+}
